9bcf43295e04fb989720bd5d3d3bc3ca6e8682a4
at0mic <dan@at0mic.com>
2018-05-02 15:29:04 -0500

    made dictionary public

---
+++ diccionario.js
@@ -0,0 +1,21 @@
+diccionario = [
+  'pan',
+  'pri',
+  'prd',
+  'morena',
+  'amlo',
+  'anaya',
+  'meade',
+  'populismo',
+  'populista',
+  'populistas',
+  'debate',
+  'senado',
+  'lopez obrador',
+  'lópez obrador',
+  'prian',
+  'peña nieto',
+  'salinas',
+  'vicente fox',
+  'elecciones'
+];
